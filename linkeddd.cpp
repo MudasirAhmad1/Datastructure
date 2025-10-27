b2da5782db084ff458d9de6ff56f695f276170de
mudasir <mudasirahmadmca@gmail.com>
2025-10-27 12:15:25 +0530

    Mudasir

---
+++ linkeddd.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+	int data;
+	struct node *next;
+};
+int main()
+{
+	int n;
+	printf("Enter the value:");
+	scanf("%d",&n);
+	struct node *head=NULL;
+	struct node *last=NULL;
+	for(int i=0;i<n;i++)
+	{
+		int value;
+		printf("Enter the Value of Node:");
+		scanf("%d",&value);
+		struct node *newnode=(struct node *)malloc(sizeof(struct node));
+		newnode->data=value;
+		newnode->next=NULL;
+		if(head==NULL)
+		{
+			head=newnode;
+			last=head;
+		}
+		else
+		{
+			last->next=newnode;
+			last=newnode;
+		}
+	}
+	struct node *p=head;
+	while(p!=NULL)
+	{
+		printf("%d",p->data);
+		p=p->next;
+	}
+}
