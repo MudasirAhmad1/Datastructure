6acd145b4556cf9d16e38b02d0983f6d6ad463c1
mudasir <mudasirahmadmca@gmail.com>
2025-10-28 09:52:42 +0530

    Mudasir

---
+++ cricullar.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+		int data;
+		struct node *next;
+		struct node *prev;
+	};
+int main()
+{
+	int n;
+	printf("Enter the Number of Nodes:");
+	scanf("%d",&n);
+	struct node *head=NULL;
+	struct node *last=NULL;
+	for(int i=0;i<n;i++)
+	{
+		int value;
+		printf("Enter the value:");
+		scanf("%d",&value);
+		struct node *newnode=(struct node *)malloc(sizeof(struct node));
+		newnode->data=value;
+		newnode->next=NULL;
+		newnode->prev=NULL;
+		if(head==NULL)
+		{
+			head=newnode;
+			last=head;
+			head->prev=last;
+			last->next=head;
+		}
+		else
+		{
+			last->next=newnode;
+			newnode->prev=last;
+			newnode->next=head;
+			head->prev=newnode;
+			last=newnode;
+		}
+	}
+}
---
+++ sortedlik.cpp
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+	int data;
+	struct node *next;
+	struct node *prev;
+};
+int main()
+{
+	int n;
+	printf("Enter the value:");
+	scanf("%d",&n);
+	struct node *head=NULL;
+	struct node *last=NULL;
+	struct node *prev=NULL;	
+	for(int i=0;i<n;i++)
+	{
+		int value;
+		printf("Enter the Value of Node:");
+		scanf("%d",&value);
+		struct node *newnode=(struct node *)malloc(sizeof(struct node));
+		newnode->data=value;
+		newnode->next=NULL;
+		newnode->prev=NULL;
+		if(head==NULL)
+		{
+			head=newnode;
+			last=head;
+			head->prev=last;
+		}
+		else
+		{
+			last->next=newnode;
+			newnode->prev=last;
+			last=newnode;
+			
+		}
+	}
+	
+		struct node *firstnode=(struct node *)malloc(sizeof(struct node));
+		firstnode->data=77;
+		firstnode->next=NULL;
+		firstnode->prev=NULL;
+		
+		firstnode->next=head;
+		firstnode->prev=head;
+		head=firstnode;
+		
+		int x=-32768;
+		struct node *q=head;
+		while(q!=NULL)
+		{
+			if(q->data<x)
+			{
+			printf("Sorted");	
+			}
+			else
+			{
+				x=p->data;
+				p=p->next;
+				printf("Not Sorted");
+			}
+			
+		}
+	
+	struct node *p=head;
+	while(p!=NULL)
+	{
+		printf("%d ",p->data);
+		p=p->next;
+	}
+}
