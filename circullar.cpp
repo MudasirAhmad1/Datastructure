671f931ce63eb221640f800d97e558b9d82a8106
mudasir <mudasirahmadmca@gmail.com>
2025-10-29 20:39:04 +0530

    Muzamil

--- circullar.cpp
+++ circullar.cpp
@@ -14,4 +14,18 @@ int main()
 	struct node *last=NULL;
 	for(int i=0;i<n;i++)
 	{
+		int value;
+		printf("Enter the value:");
+		scanf("%d",&value);
+		struct node *newnode=(struct node *)malloc(sizeof(struct node));
+		newnode->data=value;
+		newnode->next=NULL;
+		newnode->prev=NULL;
+		if(head==NULL)
+		{
+			head=newnode;
+			last=head;
+			head->prev=last;
+			last->next=head;
+		}
 	
