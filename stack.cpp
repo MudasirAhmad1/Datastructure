fd66058a809a62ce10f2cfaa16897006077842de
mudasir <mudasirahmadmca@gmail.com>
2025-10-30 13:05:28 +0530

    Muzamil

--- stack.cpp
+++ stack.cpp
@@ -1,6 +1,22 @@
 #include<stdio.h>
 #define size=5;
+int stack[size];
+int top=-1;
+void push(int value)
+{
+	if(top==size-1)
+	printf("Stack is Full\n");
+	else
+	stack[++top]=value;
+}
+
+
 int main()
 {
+	int a[size];
+	int top=-1;
+	
+	
+	
 	printf("The world is good:");
 }
