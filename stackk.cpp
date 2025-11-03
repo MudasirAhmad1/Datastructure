893be2cc77c9286fdb6592a96d69e4be24f5d87c
mudasir <mudasirahmadmca@gmail.com>
2025-11-03 09:53:24 +0530

    Muzamil

--- stackk.cpp
+++ stackk.cpp
@@ -1,44 +1,5 @@
 #include<stdio.h>
-#include<conio.h>
 #define size 5
 int stack[size];
 int top=-1;
-void push(int value)
-{
-	if(top==size-1)
-	{
-		printf("The stack is full:");
-	}
-	else
-	{
-		stack[++top]=value;
-	}
-}
-	void pop()
-	{
-		if(top==-1)
-		{
-			printf("stack is empty");
-		}
-		else
-		{
-			printf("popped %d",stack[top--]);
-		}
-	}
-	void peek()
-	{
-		if(top==-1)
-		{
-			printf("stck is empty");
-		}
-		else
-		{
-			printf("top emeplent: %d",stack[top]);
-		}
-	}
-	int main()
-	{
-		push(10);
-		push(30);
-		pop();
-	}
+
