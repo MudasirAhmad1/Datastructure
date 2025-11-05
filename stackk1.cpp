eba48e34a9bdd7c450b4d9fe2c330548e235e58f
mudasir <mudasirahmadmca@gmail.com>
2025-11-05 10:03:32 +0530

    Muzamil

--- stackk1.cpp
+++ stackk1.cpp
@@ -16,13 +16,43 @@ void push(int value)
 void pop()
 {
 	struct node *temp=top;
-	printf("The elemet is popped:%d",top->data);
+	printf("\n The elemet is popped:%d",top->data);
 	top=top->next;
 	free(temp);
+}
+void peek()
+{
+	if(top==NULL)
+	{
+		printf("The stack is Empty:");
+	}
+	else
+	{
+		printf("\n The stack is data:%d",top->data);
+	}
+}
+
+void display()
+{
+	struct node *p=top;
+	if(p=NULL)
+	{
+		printf("Stack is Empty:");
+		return;
+	}
+	while(p!=NULL)
+	{
+		printf("\n The data is %d",p->data);
+		p=p->next;
+		printf("Null \n");
+	}
 }
 int main()
 {
 	push(34);
 	push(12);
+	push(34);
 	pop();
+	peek();
+	display();
 }
