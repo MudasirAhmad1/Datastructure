cdf4357622d8a1ec84a3320447a92bd403e54b23
mudasir <mudasirahmadmca@gmail.com>
2025-11-06 13:21:25 +0530

    Muzamil

---
+++ staack.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int value)
+{
+struct node *newnode=(struct node *)malloc(sizeof(struct node));	
+newnode->data=value;
+newnode->next=top;
+top=newnode;
+printf("The Number is Pushed into the stack %d",value);
+}
+void pop()
+{
+	struct node *temp=top;
+	top=top->next;
+	free(temp);
+	printf("The Number pop")
+}
+
+
+
