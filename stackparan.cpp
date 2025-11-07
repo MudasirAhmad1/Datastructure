d3a1508bb41e2a136c1af0eddff68882a1abca23
mudasir <mudasirahmadmca@gmail.com>
2025-11-07 09:54:37 +0530

    Muzamil

--- stackparan.cpp
+++ stackparan.cpp
@@ -1,36 +1,45 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
 struct node {
-	char data;
-	struct node *next;
+    char data[30];
+    struct node *next;
 };
-struct node *top=NULL;
-void push(char value[])
-{
-	struct node *newnode=(struct node *)malloc(sizeof(struct node));
-	strcpy(newnode->data, value);
-//	newnode->data=value;
-	newnode->next=top;
-	top=newnode;
-	printf("\n The Number is Pushed %s: ",value);
+
+struct node *top = NULL;
+
+void push(char value[]) {
+    struct node *newnode = (struct node *)malloc(sizeof(struct node));
+    if (newnode == NULL) {
+        printf("Memory allocation failed!\n");
+        return;
+    }
+
+    strcpy(newnode->data, value);
+    newnode->next = top;
+    top = newnode;
+
+    printf("\n'%s' is pushed onto the stack", value);
 }
-void pop()
-{
-if(top==NULL)
-{
-	printf("\n The Stack is Empty: ");
+
+void pop() {
+    if (top == NULL) {
+        printf("\nThe Stack is Empty");
+    } else {
+        struct node *temp = top;
+        printf("\n'%s' is popped from the stack", top->data);
+        top = top->next;
+        free(temp);
+    }
 }
-else
-{
-	struct node *temp=top;
-	printf("\n The Number is Popped %s",top->data);
-	top=top->next;
-	free(temp);
+
+int main() {
+    push("Muzamil");
+    push("Mudasir");
+    pop();
+    pop();
+    pop(); // test empty
+    return 0;
 }
-}
-int main(){
-	push("Mudasir");
-	push("Muzamil");
-	pop();
-}
+
