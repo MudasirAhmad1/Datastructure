f20da7c6ae4aa96b7f3e41a6aec991a46a7e3716
mudasir <mudasirahmadmca@gmail.com>
2025-11-07 11:56:53 +0530

    Muzamil

--- stackparan.cpp
+++ stackparan.cpp
@@ -1,64 +1,66 @@
-#include<stdio.h>
-#include<Stdlib.h>
-struct node{
-	char data;
-	struct node *next;
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    char data;
+    struct node *next;
 };
-struct node *top=NULL;
-void push(char value)
-{
-	struct node *newnode=(struct node *)malloc(sizeof(struct node));
-	if(newnode==NULL)
-	{
-		printf("The Stack is Empty:");
-	}
-	else
-	{
-		newnode->data=value;
-		printf("\n The Number is Pushed into Stack:%c",value);
-		newnode->next=top;
-		top=newnode;
-	}
+
+struct node *top = NULL;
+
+void push(char value) {
+    struct node *newnode = (struct node *)malloc(sizeof(struct node));
+    if (newnode == NULL) {
+        printf("Memory allocation failed.\n");
+        exit(1); // Stop in case of serious error
+    }
+    newnode->data = value;
+    newnode->next = top;
+    top = newnode;
 }
-void pop()
-{
-	if(top==NULL)
-	{
-		printf("The Stack is Empty:");
-	}
-	else
-	{
-		struct node *temp=top;
-		printf("\n The Number is Popped %c",top->data);
-		top=top->next;
-		free(temp);
-	}
+
+void pop() {
+    if (top == NULL) {
+        // Nothing to pop
+        return;
+    }
+    struct node *temp = top;
+    top = top->next;
+    free(temp);
 }
-int balance(char *exp)
-{
-	int i;
-	for(i=0;exp[i]!='\0';i++)
-	{
-		if(exp[i]=='(')
-		push(exp[i]);
-		else if(exp[i]==')')
-		{
-			if(top==NULL)
-			{
-				return 0;	
-			}
-			else
-			pop();
-		}
-	}
-	if(top==NULL)
-	return 1;
-	else
-	return 0;
+
+void clear_stack() {
+    while (top != NULL) {
+        pop();
+    }
 }
-int main()
-{
-	char *exp="((a+b)*(c+d)";
-	printf("\n %d is balanced",balance(exp));
-	return 0;
+
+// Returns 1 if balanced, 0 otherwise
+int balance(char *exp) {
+    int i;
+    for (i = 0; exp[i] != '\0'; i++) {
+        if (exp[i] == '(')
+            push(exp[i]);
+        else if (exp[i] == ')') {
+            if (top == NULL) {
+                clear_stack(); // To avoid memory leak
+                return 0;
+            } else
+                pop();
+        }
+    }
+    int is_balanced = (top == NULL);
+    clear_stack();
+    return is_balanced;
 }
+
+int main() {
+    char *exp = "((a+b)*(c+d))";
+    if (balance(exp)) {
+        printf("\nThe expression is balanced\n");
+    } else {
+        printf("\nThe expression is NOT balanced\n");
+    }
+    return 0;
+}
+
