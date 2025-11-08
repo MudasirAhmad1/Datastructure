bc0425276c650795dd195c40e2cad7911c89b28f
mudasir <mudasirahmadmca@gmail.com>
2025-11-08 22:45:46 +0530

    Muzamil

--- 555.cpp
+++ 555.cpp
@@ -49,5 +49,19 @@ struct node {
 	struct node *next;
 };
 struct node *top=NULL;
+void push(int )#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
 void push(int )
 void push(int )
