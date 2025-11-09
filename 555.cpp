f4f70f36202a72fc637b17e378536e172d5ba0ae
mudasir <mudasirahmadmca@gmail.com>
2025-11-09 19:05:38 +0530

    Muzamil

--- 555.cpp
+++ 555.cpp
@@ -648,7 +648,299 @@ void push(int )
 struct node {
 	int data;
 	struct node *next;
+};#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
 };
+struct node *top=NULL;
+void push(int )
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node *next;
+};
+struct node *top=NULL;
+void push(int )
 struct node *top=NULL;
 void push(int )
 struct node *top=NULL;
