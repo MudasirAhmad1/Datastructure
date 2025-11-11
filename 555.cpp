d45afd7fe983c7cb2eedf8ccd86e80dede8ae0bb
mudasir <mudasirahmadmca@gmail.com>
2025-11-11 16:47:28 +0530

    Muzamil

--- 555.cpp
+++ 555.cpp
@@ -15,20 +15,3 @@ int main()
 	change(c1);
 	printf("\n The cricle is : %d %d",c1.l,c1.b);
 }
-#include<stdio.h>
-struct cricle{
-	int l=56;
-	int b=78;
-};
-void change(struct cricle c2)
-{
-	c2.l=9;
-	printf("\n The 2 cricle is %d %d",c2.l,c2.b);
-}
-int main()
-{
-	struct cricle c1;
-	
-	change(c1);
-	printf("\n The cricle is : %d %d",c1.l,c1.b);
-}
