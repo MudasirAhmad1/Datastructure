33b9138479015cbd2f37b3261364db6432cb56e4
mudasir <mudasirahmadmca@gmail.com>
2025-11-14 11:31:56 +0530

    Muzamil

--- 555.cpp
+++ 555.cpp
@@ -63,4 +63,199 @@ int main()
 	
 	change(c1);
 	printf("\n The cricle is : %d %d",c1.l,c1.b);
+}#include<stdio.h>
+struct cricle{
+	int l=56;
+	int b=78;
+};
+void change(struct cricle c2)
+{
+	c2.l=9;
+	printf("\n The 2 cricle is %d %d",c2.l,c2.b);
+}
+#include<stdio.h>
+struct cricle{
+	int l=56;
+	int b=78;
+};
+void change(struct cricle c2)
+{
+	c2.l=9;
+	printf("\n The 2 cricle is %d %d",c2.l,c2.b);
+}
+int main()
+{
+	struct cricle c1;
+	
+	change(c1);
+	printf("\n The cricle is : %d %d",c1.l,c1.b);
+}#include<stdio.h>
+struct cricle{
+	int l=56;
+	int b=78;
+};
+void change(struct cricle c2)
+{
+	c2.l=9;
+	printf("\n The 2 cricle is %d %d",c2.l,c2.b);
+}
+int main()
+{
+	struct cricle c1;
+	
+	change(c1);
+	printf("\n The cricle is : %d %d",c1.l,c1.b);
+}#include<stdio.h>
+struct cricle{
+	int l=56;
+	int b=78;
+};
+void change(struct cricle c2)
+{
+	c2.l=9;
+	printf("\n The 2 cricle is %d %d",c2.l,c2.b);
+}
+int main()
+{
+	struct cricle c1;
+	
+	change(c1);
+	printf("\n The cricle is : %d %d",c1.l,c1.b);
+}
+int main()
+{
+	struct cricle c1;
+	
+	change(c1);
+	printf("\n The cricle is : %d %d",c1.l,c1.b);
+}#include<stdio.h>
+struct cricle{
+	int l=56;
+	int b=78;
+};
+void change(struct cricle c2)
+{
+	c2.l=9;
+	printf("\n The 2 cricle is %d %d",c2.l,c2.b);
+}
+#include<stdio.h>
+struct cricle{
+	int l=56;
+	int b=78;
+};
+void change(struct cricle c2)
+{
+	c2.l=9;
+	printf("\n The 2 cricle is %d %d",c2.l,c2.b);
+}
+int main()
+{
+	struct cricle c1;
+	
+	change(c1);
+	printf("\n The cricle is : %d %d",c1.l,c1.b);
+}#include<stdio.h>
+struct cricle{
+	int l=56;
+	int b=78;
+};
+void change(struct cricle c2)
+{
+	c2.l=9;
+	printf("\n The 2 cricle is %d %d",c2.l,c2.b);
+}
+int main()
+{
+	struct cricle c1;
+	
+	change(c1);
+	printf("\n The cricle is : %d %d",c1.l,c1.b);
+}#include<stdio.h>
+struct cricle{
+	int l=56;
+	int b=78;
+};
+void change(struct cricle c2)
+{
+	c2.l=9;
+	printf("\n The 2 cricle is %d %d",c2.l,c2.b);
+}
+int main()
+{
+	struct cricle c1;
+	
+	change(c1);
+	printf("\n The cricle is : %d %d",c1.l,c1.b);
+}
+int main()
+{
+	struct cricle c1;
+	
+	change(c1);
+	printf("\n The cricle is : %d %d",c1.l,c1.b);
+}#include<stdio.h>
+struct cricle{
+	int l=56;
+	int b=78;
+};
+void change(struct cricle c2)
+{
+	c2.l=9;
+	printf("\n The 2 cricle is %d %d",c2.l,c2.b);
+}
+#include<stdio.h>
+struct cricle{
+	int l=56;
+	int b=78;
+};
+void change(struct cricle c2)
+{
+	c2.l=9;
+	printf("\n The 2 cricle is %d %d",c2.l,c2.b);
+}
+int main()
+{
+	struct cricle c1;
+	
+	change(c1);
+	printf("\n The cricle is : %d %d",c1.l,c1.b);
+}#include<stdio.h>
+struct cricle{
+	int l=56;
+	int b=78;
+};
+void change(struct cricle c2)
+{
+	c2.l=9;
+	printf("\n The 2 cricle is %d %d",c2.l,c2.b);
+}
+int main()
+{
+	struct cricle c1;
+	
+	change(c1);
+	printf("\n The cricle is : %d %d",c1.l,c1.b);
+}#include<stdio.h>
+struct cricle{
+	int l=56;
+	int b=78;
+};
+void change(struct cricle c2)
+{
+	c2.l=9;
+	printf("\n The 2 cricle is %d %d",c2.l,c2.b);
+}
+int main()
+{
+	struct cricle c1;
+	
+	change(c1);
+	printf("\n The cricle is : %d %d",c1.l,c1.b);
+}
+int main()
+{
+	struct cricle c1;
+	
+	change(c1);
+	printf("\n The cricle is : %d %d",c1.l,c1.b);
 }
