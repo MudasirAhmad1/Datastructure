50df0326263f43de1ecfb6136f60d05886eb95d4
mudasir <mudasirahmadmca@gmail.com>
2025-11-18 11:34:34 +0530

    my commit

--- stackk2.cpp
+++ stackk2.cpp
@@ -6,11 +6,11 @@ void push(int value)
 {
 	if(top==size-1)
 	{
-		printf("\n The size of the stack is full:");
+		printf("\n The Stack is Full");
 	}
 	else
 	{
-		printf("\n The Number is pushed into the stack: %d",value);
+		printf("\n The Number is Has been pushed into the stack %d",value);
 		stack[++top]=value;
 	}
 }
@@ -18,29 +18,17 @@ void pop()
 {
 	if(top==-1)
 	{
-		printf("\n The Stack is Empty:");
-	}
-	else
-	{
-		printf("\n The Element is Popped is %d",stack[top--]);
-	}
-}
-void peek()
-{
-	if(top==-1)
-	{
 		printf("\n The stack is Empty");
 	}
-	else
-	{
-		printf("\n Top Element is %d",stack[top]);
+	else{
+	printf("\n The number is has been Popped %d",stack[top--]);	
 	}
 }
 int main()
 {
-	push(34);
-	push(43);
-	push(32);
+	push(1);
+	push(56);
+	push(2);
 	pop();
-	peek();
+	
 }
