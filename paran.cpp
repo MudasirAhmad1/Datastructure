79a2019d422cec989facdc46cd8269dfe7e83b95
mudasir <mudasirahmadmca@gmail.com>
2025-11-26 21:25:07 +0530

    Muzamil

--- paran.cpp
+++ paran.cpp
@@ -23,7 +23,9 @@ void pop()
 	}
 	else
 	{
-		printf("\n The Char is popped out:%c",);
+//		printf("\n The Char is popped out:%c",);
+//		printf("\n The Char is popped out:%c",);
+//		printf("\n The Char is popped out:%c",);
 		stack[top--];
 	}
 }
