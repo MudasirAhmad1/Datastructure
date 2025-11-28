53c9cca2447c3cbd3c76388a24c57868709f625e
mudasir <mudasirahmadmca@gmail.com>
2025-11-28 11:48:20 +0530

    Muzamil

---
+++ Stackee.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#define size 5
+int stack[size];
+int top=-1;
+void push(int value)
+{
+	if(top==size-1)
+	{
+		printf("\n The STack is Full:");
+	}
+	else
+	{
+		printf("\n The Element is Pushed into the stack:%d",value);
+		stack[++top]=value;	
+	}
+}
+void pop()
+{
+	if(top==-1)
+	{
+		printf("\n The Stack is Empty:");
+	}
+	else
+	{
+		printf("\n The Element is popped out %d",stack[top--]);
+	}
+}
+int main()
+{
+	push(45);
+	push(25);
+	pop();
+}
