146a6642fe9e7a6441f7bab30442664ad8446dd2
mudasir <mudasirahmadmca@gmail.com>
2025-11-28 11:51:28 +0530

    Muzamil

--- Stackee.cpp
+++ Stackee.cpp
@@ -25,9 +25,20 @@ void pop()
 		printf("\n The Element is popped out %d",stack[top--]);
 	}
 }
+void peek()
+{
+	if(top==-1){
+		printf("\n The stack is Empty:");
+	}
+	else
+	{
+		printf("\n The top Eleement is %d",stack[top]);
+	}
+}
 int main()
 {
 	push(45);
 	push(25);
 	pop();
+	peek();
 }
