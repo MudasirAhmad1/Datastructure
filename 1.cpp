b33d655d34f19164b7c4b954f25fef5183069024
mudasir <mudasirahmadmca@gmail.com>
2025-11-29 22:53:19 +0530

    Muzamil

---
+++ 1.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+struct node{
+	int data;
+	struct node *next;
+};
+int main()
+{
+	struct node *head=NULL;
+	struct node *second=NULL;
+	struct node *third=NULL;
+	head=(struct node *)malloc(sizeof(struct node));
+	second=(struct node *)malloc(sizeof(struct node));
+	third=(struct node *)malloc(sizeof(struct node));
+	head->data=45;
+	head->next=second;
+	second->data=56;
+	second->next=third;
+	third->data=23;
+	
+	struct node *p=head;
+	while(p!=NULL)
+	{
+		printf("The Number is %d",p->data);
+		
+	}
+}
