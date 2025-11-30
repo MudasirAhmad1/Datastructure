619d5ebf2d06f2af5be6c0ee58be4d1ea9443829
mudasir <mudasirahmadmca@gmail.com>
2025-11-30 09:44:35 +0530

    Muzamil

--- 1.cpp
+++ 1.cpp
@@ -1,38 +1,15 @@
 #include<stdio.h>
 #include<conio.h>
-#include<stdlib.h>
-struct node{
-	int data;
-	struct node *next;
-};
+int swap(int a,int b)
+	{
+		return a+b;
+	}
 int main()
 {
-	struct node *head=NULL;
-	struct node *second=NULL;
-	struct node *third=NULL;
-	head=(struct node *)malloc(sizeof(struct node));
-	second=(struct node *)malloc(sizeof(struct node));
-	third=(struct node *)malloc(sizeof(struct node));
-	head->data=45;
-	head->next=second;
-	second->data=56;
-	second->next=third;
-	third->data=23;
-	third->next=NULL;
-	
-	struct node *newnode=(struct node *)malloc(sizeof(struct node));
-	newnode->data=90;
-	third->next=newnode;
-	newnode->next=NULL;
-	
-
-	
-	
-	struct node *p=head;
-	while(p!=NULL)
-	{
-		printf("\n The Number is %d",p->data);
-		p=p->next;
-		
-	}
+	int a=34;
+	int b=55;
+	int result;
+	result=swap(4,5);
+	printf("The result is :%d",result);
 }
+
