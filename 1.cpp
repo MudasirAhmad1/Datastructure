e210ddb4af9b43e169c5996235a2429a83e654eb
mudasir <mudasirahmadmca@gmail.com>
2025-11-30 11:10:32 +0530

    Muzamil

--- 1.cpp
+++ 1.cpp
@@ -1,26 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    int *arr;
-    int n;
-
-    printf("Enter size: ");
-    scanf("%d", &n);
-
-    arr = (int*)malloc(n * sizeof(int));  // create array in heap
-
-    // take input
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    // print array
-    for(int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-
-    free(arr); // free heap memory
-    return 0;
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+int main()
+{
+	int *arr;
+	int n;
+	printf("\n Enter the Elements of the array:");
+	scanf("%d",&n);
+	arr=(int *)malloc(n*sizeof(int));
+	for(int i=0;i<n;i++)
+	{
+		printf("Enter the NUmber:");
+		scanf("%d", &arr[i]);
+	}
+	
+	for(int i=0;i<n;i++)
+	{
+		printf("%d",arr[i]);
+	}
+	
 }
-
