2c0cfa536e7c7da1ae738b78b31e43171e02dabd
mudasir <mudasirahmadmca@gmail.com>
2025-11-30 10:16:05 +0530

    Muzamil

--- 1.cpp
+++ 1.cpp
@@ -1,13 +1,26 @@
-#include<stdio.h>
-void sum(int a[])
-{
-for(int i=0;i<6;i++)
-{
-	printf("\n %d",a[i]);
-	}	
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *arr;
+    int n;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    arr = (int*)malloc(n * sizeof(int));  // create array in heap
+
+    // take input
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // print array
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    free(arr); // free heap memory
+    return 0;
 }
-int main()
-{
-	int a[]={1,2,3,4,5,5};
-	sum(a);
-}
+
