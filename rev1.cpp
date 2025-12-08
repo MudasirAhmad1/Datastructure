002dda419b711bee9aa21830f5b5e40f9f3c7f84
mudasir <mudasirahmadmca@gmail.com>
2025-12-08 12:46:51 +0530

    Muzamil

--- rev1.cpp
+++ rev1.cpp
@@ -1,16 +1,15 @@
 #include<stdio.h>
+int add(int n)
+{
+	if(n>0)
+	{
+		add(n-1);
+		printf("%d: ",n);
+		
+	}
+}
 int main()
 {
-	int a[10]={1,2,3,5,6,7,8,9,10};
-	int size=9;
-	int add=67;
-	int index=99;
-	if(2<index)
-	{
-		printf("True");
-	}
-	else
-	{
-		printf("False");
-	}
+	int n=9;
+	add(n);
 }
